a58bf84e239c83a66c38e9daac3301b22112b2dc
byaaminee <byaaminee@gmail.com>
2024-07-19 14:24:10 -0400

    Add final project code

---
+++ src/main/java/com/QuebecBonds/FinalProject/App.java
@@ -0,0 +1,13 @@
+package com.QuebecBonds.FinalProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
